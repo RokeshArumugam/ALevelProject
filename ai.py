186840d0903062b2976fa33c0e06ef14f1a405c0
Rokesh Arumugam <74315313+RokeshArumugam@users.noreply.github.com>
2020-12-12 12:08:39 +0000

    Shortened ai.py

--- ai.py
+++ ai.py
@@ -1,28 +1,15 @@
 import cv2
-import sys
 import pytesseract
 
-if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		print('Usage: python ocr_simple.py image.jpg')
-		sys.exit(1)
-	
-	# Read image path from command line
-	imPath = sys.argv[1]
-	
+def readText(imgPath):
 	# Uncomment the line below to provide path to tesseract manually
 	# pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 	
-	# Define config parameters.
-	# '-l eng'	  for using the English language
-	# '--oem 1' for using LSTM OCR Engine
-	config = ('-l eng --oem 1 --psm 3')
-	
 	# Read image from disk
-	im = cv2.imread(imPath, cv2.IMREAD_COLOR)
+	im = cv2.imread(imgPath, cv2.IMREAD_COLOR)
 	
 	# Run tesseract OCR on image
-	text = pytesseract.image_to_string(im, config=config)
-	
-	# Print recognized text
+	# '-l eng'	  for using the English language
+	# '--oem 1' for using LSTM OCR Engine
+	text = pytesseract.image_to_string(im, config=('-l eng --oem 1 --psm 3'))
 	print(text)
