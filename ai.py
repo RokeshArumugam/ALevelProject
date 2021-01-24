9c46d3a6d3f0eda6154d76b4a4c3ee133e5446e9
Rokesh Arumugam <74315313+RokeshArumugam@users.noreply.github.com>
2021-01-24 11:15:27 +0000

    Reformat ai.py
    Condensed readText function into one line, spread over multiple lines
    using escaped newline. This means there are no onetime use variables.
    
    Separated formatText function into multiple lines

--- ai.py
+++ ai.py
@@ -7,15 +7,20 @@ def readText(imgData):
 	# Uncomment the line below to provide path to tesseract manually
 	# pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 	
-	# Read image from base64 string
-	nparr = np.fromstring(base64.b64decode(imgData), np.uint8)
-	img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-	
 	# Run tesseract OCR on image
 	# '-l eng'	  for using the English language
 	# '--oem 1' for using LSTM OCR Engine
-	return formatText(pytesseract.image_to_string(img, config=('-l eng --oem 1 --psm 3')))
+	return formatText(
+		pytesseract.image_to_string(
+			cv2.imdecode(
+				np.fromstring(base64.b64decode(imgData), np.uint8),
+				cv2.IMREAD_COLOR
+			),
+			config=('-l eng --oem 1 --psm 3')
+		)
+	)
 
 def formatText(text):
-	text = re.sub(r"([^.!?\n])\n([^\n])", r"\1 \2", text[:-1])
+	text = text[:-1]
+	text = re.sub(r"([^.!?\n])\n([^\n])", r"\1 \2", text)
 	return text
